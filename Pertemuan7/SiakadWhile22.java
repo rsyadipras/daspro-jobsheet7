5d6e1bb4017389acd0a093e900cb8164641833e5
rsyadipras <rsyadipras@gmail.com>
2024-10-10 08:42:21 +0700

    P7_Percobaan2_PertanyaanNo4

--- Pertemuan7/SiakadWhile22.java
+++ Pertemuan7/SiakadWhile22.java
@@ -24,6 +24,7 @@ public class SiakadWhile22 {
             }
             if (nilai > 80 && nilai <= 100) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah A"); // 85
+                System.out.println("Bagus, pertahankan nilainya!");
             } else if (nilai > 73 && nilai <= 80) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B+"); // 63
             } else if (nilai > 65 && nilai <= 73) {
